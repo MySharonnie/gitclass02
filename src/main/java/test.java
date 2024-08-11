dbc5436823bc05ba422007dbc9bdce7d1dd0b107
MySharonnie <sharonneblevins@gmail.com>
2024-08-11 09:23:06 -0400

    2nd Commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,8 +3,9 @@ public class test {
     public static void main(String[] args) {
         System.out.println("I love Java!");
         System.out.println("Hello World");
-        System.out.println("This is project version 1");
+        System.out.println("This is project version 2");
+        System.out.println("entering in new code");
     }
 }
 
-//done with version 1
+//done with version 2
