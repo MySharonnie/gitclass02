f2499f05f2f7053b01457f588fd35fac7eace24b
MySharonnie <sharonneblevins@gmail.com>
2024-08-11 09:12:22 -0400

    1stcommit

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("I love Java!");
+        System.out.println("Hello World");
+        System.out.println("This is project version 1");
+    }
+}
+
+//done with version 1
